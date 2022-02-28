963421975f3b188c52d48b661b70c92d234e923f
rishat <rishatrifa56@gmail.com>
2022-02-28 23:22:16 +0600

    no phone found message added

--- js/api.js
+++ js/api.js
@@ -3,7 +3,20 @@ const searchPhone = () => {
     //console.log(inputValue);
     fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => cardDisplay(data))
 
     document.getElementById('input-value').value = '';
+
+}
+
+const cardDisplay = (cards) => {
+    if (!cards.status) {
+        document.getElementById('phone-not-found').innerText = 'No phone found';
+
+    }
+    else {
+        cards = cards.data;
+        console.log(cards);
+    }
+
 }
