306b2ba5c5abfe551e70ee6496c66438be729d9b
rishat <rishatrifa56@gmail.com>
2022-02-28 22:35:09 +0600

    phone site load done

--- js/api.js
+++ js/api.js
@@ -1,1 +1,9 @@
-console.log("hello r");
+const searchPhone = () => {
+    const inputValue = document.getElementById('input-value').value;
+    //console.log(inputValue);
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+
+    document.getElementById('input-value').value = '';
+}
