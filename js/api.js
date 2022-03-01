14023289734abdd9291ae2394c6c81d805b35301
rishat <rishatrifa56@gmail.com>
2022-03-01 22:09:03 +0600

    spinner added

--- js/api.js
+++ js/api.js
@@ -2,10 +2,19 @@
 const main = document.getElementById('main');
 const detailShow = document.getElementById('detail-show');
 
+// spinner show 
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+
 const searchPhone = () => {
     document.getElementById('main').innerHTML = '';
     document.getElementById('detail-show').innerHTML = '';
     const inputValue = document.getElementById('input-value').value;
+    // spinner display 
+    toggleSpinner('block');
+
+
     fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
         .then(response => response.json())
         .then(data => cardDisplay(data))
@@ -17,11 +26,11 @@ const searchPhone = () => {
 const cardDisplay = (cards) => {
     if (!cards.status) {
         document.getElementById('phone-not-found').innerText = 'No phone found';
+        toggleSpinner('none');
 
     }
     else {
         document.getElementById('phone-not-found').innerText = '';
-        // console.log(cards.data);
         cards = cards.data;
         // handle at-most 20 data or phone
         if (cards.length > 20) {
@@ -33,7 +42,7 @@ const cardDisplay = (cards) => {
             div.classList.add("col-lg-4");
             div.classList.add('mb-5');
             div.innerHTML = `
-            <div class="card" style="width: 18rem;">
+            <div class="card">
             <img src="${card.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Name: ${card.phone_name}</h5>
@@ -45,6 +54,7 @@ const cardDisplay = (cards) => {
         `;
             main.appendChild(div);
         }
+        toggleSpinner('none');
 
     }
 
@@ -62,33 +72,33 @@ const setDetails = (phone) => {
 
     document.getElementById('detail-show').innerHTML = '';
     phone = phone.data;
-    console.log(phone);
+    //console.log(phone);
     const div = document.createElement('div');
     div.classList.add('card');
-    div.classList.add('me-2');
-
     div.innerHTML = `
         
-        <div class='w-50 mx-auto'>
+        <div class="w-50 mx-auto">
            <img src="${phone.image}" class="card-img-top" alt="...">
         </div>
         <div class="card-body">
             <h5 class="card-title">Name: ${phone.name}</h5>
-            <h6 class="card-text"><span>Release-Date:</span> ${phone.releaseDate ? phone.releaseDate : 'No release date'}</h6>
+            <br>
+            <h6 class="card-text"><span>Release-Date:</span> ${phone.releaseDate ? phone.releaseDate : 'No release date found'}</h6>
+            <br>
             <h6 class="card-text"><span>ChipSet:</span> ${phone.mainFeatures.chipSet}</h6>
+            <h6 class="card-text"><span>Display-Size:</span> ${phone.mainFeatures.displaySize}</h6>
             <h6 class="card-text"><span>Memory:</span> ${phone.mainFeatures.memory}</h6>
             <h6 class="card-text"><span>Storage:</span> ${phone.mainFeatures.storage}</h6>
-            <h6 class="card-text"><span>DisplaySize:</span> ${phone.mainFeatures.displaySize}</h6>
-
+            <br>
             <h6 class="card-text"><span>Sensors:</span> ${phone.mainFeatures.sensors.join()}</h6>
-
+            <br>
             <h6 class="card-text"><span>Others:</span>
-            ${phone.others ? `  <h6 class="card-text"><span>WLAN:</span> ${phone.others.WLAN} </h6>
-                                <h6 class="card-text"><span>Blutooth:</span> ${phone.others.Bluetooth} </h6>
-                                <h6 class="card-text"><span>GPS:</span> ${phone.others.GPS} </h6> 
-                                <h6 class="card-text"><span>NFC:</span> ${phone.others.NFC} </h6> 
-                                <h6 class="card-text"><span>Radio:</span> ${phone.others.Radio} </h6>
-                                <h6 class="card-text"><span>USB:</span> ${phone.others.USB} </h6> ` : "Others not avialable"}</h6>
+            ${phone.others ? `<h6 class="card-text"><span>WLAN:</span> ${phone.others.WLAN} </h6>
+                              <h6 class="card-text"><span>Blutooth:</span> ${phone.others.Bluetooth} </h6>
+                              <h6 class="card-text"><span>GPS:</span> ${phone.others.GPS} </h6> 
+                              <h6 class="card-text"><span>NFC:</span> ${phone.others.NFC} </h6> 
+                              <h6 class="card-text"><span>Radio:</span> ${phone.others.Radio} </h6>
+                        <h6 class="card-text"><span>USB:</span> ${phone.others.USB} </h6> ` : "Others not avialable"}</h6>
         </div>
        
         `;
